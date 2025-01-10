fcc09b5581daceb7571553ca117dd725a7ff9769
Seth Donelson <155389209+SethDonelson@users.noreply.github.com>
2025-01-09 20:22:15 -0500

    store page setup and data retrieval

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,18 @@
+import { Row, Col } from "react-bootstrap"
+import storeItems from "../data/items.json"
+import { StoreItem } from "../components/StoreItem"
+
+//pull and display each item from json data file
 export function Store(){
-    return <h1>Store</h1>
+    return (
+    <>
+    <h1>Store</h1>
+    <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map(item => (
+            <Col key={item.id}><StoreItem {...item} />
+            </Col>
+        ))}
+    </Row>
+    </>
+    )
 }
