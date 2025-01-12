c888e3b7803bbd93d07c1ec1dd5f7139305c811d
Seth Donelson <155389209+SethDonelson@users.noreply.github.com>
2025-01-12 18:29:44 -0500

    add local storage hook, add remove from shopping cart

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,6 +1,8 @@
 import { useShoppingCart } from "../context/shoppingCartContext"
 import storeItems from "../data/items.json"
 import { Stack } from "rsuite"
+import { formatCurrency } from "../utilities/formatCurrency"
+import { Button } from "react-bootstrap"
 
 type CartItemProps = {
     id: number
@@ -13,8 +15,22 @@ export function CartItem ({ id, quantity }: CartItemProps){
     if (item == null) return null
 
     return (
-        <Stack direction="horizontal" gap={2}>
-            <img src={item.imgUrl} style={{ width: "125px", height: "75px", objectFit: "cover"}} />
+        <Stack direction="horizontal" gap={2} className="d-flex align-items-center justify-content-around mb-2">
+            <img src={item.imgUrl} style={{ width: "125px", height: "100px", objectFit: "cover"}} />
+            <div className="me-auto">
+                <div>
+                    {item.name}{' '} 
+                    {quantity > 1 && (
+                    <span  className="text-muted"  style={{fontSize: "0.65rem"}}> 
+                        x{quantity}
+                    </span>
+                    )}
+                </div>
+                <div className="text-muted" style={{ fontSize: "0.75rem"}}> {formatCurrency(item.price)}
+                </div>
+            </div>
+            <div className="ml-5"> {formatCurrency(item.price * quantity)}</div>
+            <Button className="ml-5" variant="outline-danger" size="sm" onClick={() => removeFromCart(item.id)}>&times;</Button>
         </Stack>
     )
 }
--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,7 +1,9 @@
-import { Offcanvas } from  "react-bootstrap"
+import { NavItem, Offcanvas } from  "react-bootstrap"
 import { useShoppingCart } from "../context/shoppingCartContext"
 import { CartItem } from "./CartItem"
 import { Stack } from "rsuite"
+import { formatCurrency } from "../utilities/formatCurrency"
+import storeItems from "../data/items.json"
 
 type ShoppingCartProps = {
     isOpen: boolean
@@ -19,6 +21,13 @@ export function ShoppingCart({ isOpen }: ShoppingCartProps) {
                 {cartItems.map(item => (
                 <CartItem key={item.id} {...item} /> 
                 ))}
+                <div className="ms-auto fw-bold fs-5">
+                    Total {formatCurrency(cartItems.reduce((total, cartItem) => {
+                        const item = storeItems.find(i => i.id === cartItem.id)
+                        return total + (item?.price || 0) * cartItem.quantity
+                }, 0)
+                )}
+                </div>
             </Stack>
         </Offcanvas.Body>
     </Offcanvas>
